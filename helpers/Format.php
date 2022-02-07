f1d85eed8afcb804cc2b39d88d725426738699ec
alamin.php <alamin.php100@gmail.com>
2022-02-08 00:16:19 +0600

    dynamic post data

---
+++ helpers/Format.php
@@ -0,0 +1,15 @@
+<?php
+    class Format{
+        
+        public function formatDate($date){
+            return date('F j, Y, g:i A', strtotime($date));
+        }
+
+        public function shortenText($text, $limit=400){
+            $text = $text."";
+            $text = substr($text, 0, $limit);
+            $text = substr($text, 0, strrpos($text, ' '));
+            $text = $text.". . .";
+            return $text;
+        }
+    }
