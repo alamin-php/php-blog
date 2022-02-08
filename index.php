20e1520c6697c4d189aee41fbd3211e062d364de
Md Al Amin Sarker <53163466+alamin-php@users.noreply.github.com>
2022-02-08 12:07:08 +0600

    pagination added

--- index.php
+++ index.php
@@ -8,10 +8,19 @@
 	$db = new Database();
 	$fm = new Format();
 ?>
+<?php 
+	$per_page = 3;
+	if(isset($_GET["page"])){
+		$page = $_GET["page"];
+	}else{
+		$page =1;
+	}
+	$start_from = ($page-1) * $per_page;
+?>
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
 <?php 
-	$query = "SELECT * FROM tbl_post ORDER BY id DESC LIMIT 3";
+	$query = "SELECT * FROM tbl_post ORDER BY id DESC LIMIT $start_from, $per_page";
 	$result = $db->select($query);
 	if($result){
 		while($post = $result->fetch_assoc()){
@@ -32,15 +41,18 @@
 			$query = "SELECT * FROM tbl_post";
 			$result = $db->select($query);
 			$total_rows = mysqli_num_rows($result);
+			$total_pages = ceil($total_rows/$per_page);
 		
-			echo "<span class='pagination'><a href='index.php?page=1'>First Page</a></span>";
-			
-			echo "<a href='index.php?page=10'>Last Page</a></span>";
+			echo "<span class='pagination'><a href='index.php?page=1'>First Page</a>";
+			for($i=1; $i <= $total_pages; $i++){
+				echo "<a href='index.php?page=$i'>$i</a>";
+			}
+			echo "<a href='index.php?page=$total_pages'>Last Page</a></span>";
 		
 	}else{
-				header("Location:404.php");
-			} 
-			?>
+		header("Location:404.php");
+	} 
+	?>
 
 		</div>
 		<?php include "inc/sidebar.php"; ?>
